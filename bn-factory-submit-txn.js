8d4f9bd5cfb830c657355e406ced66ad3af47c85
JASMEET SINGH BAGGA <jsmtbagga45@gmail.com>
2018-05-30 15:59:43 +0530

    Update bn-factory-submit-txn.js

--- bn-factory-submit-txn.js
+++ bn-factory-submit-txn.js
@@ -1,37 +1,10 @@
 'use strict';
-/**
- * Part of a course on Hyperledger Fabric: 
- * http://ACloudFan.com
- * 
- * Composer 0.19.0
- * 
- * Demostrates the use of factory received in the BN connection using
- * the getFactory( ) method to submit a transaction
- * 
- * Pre-Req
- * 1. Start the fabric
- * 2. Deploy & start airlinev7
- * 3. Start the REST Server
- * 4. Make sure there is no Flight resource with id="AE101-05-12-2019"
- *    Delete it if you find one - Remember the code for CreateFlight
- *    Transaction has the flightId hardcoded :-)
- * 
- * Demo Steps
- * 1. Use the bn-connection-util to create the connection to airlinev7
- * 2. Get the Busines Network Definition from Runtime
- * 3. Get the factory from the Business Network definition
- * 4. Create a new Transaction instance
- * 5. Set the property values in the transaction object
- * 6. Submit the transaction
- */
-
-
 
  // Constant values - change as per your needs
  const namespace = "org.fabriccare.organ";
  const transactionType = "donateOrgan";
 
-// 1. Connect to airlinev7
+// Connecting to airlinev7
 const bnUtil = require('./bn-connection-util');
 bnUtil.connect(main);
 
@@ -43,33 +16,24 @@ function main(error){
         process.exit(1);
     }
 
-    // 2. Get the Business Network Definition
+    // Getting the Business Network Definition
     let bnDef =  bnUtil.connection.getBusinessNetwork();
     console.log("2. Received Definition from Runtime: ",
                    bnDef.getName(),"  ",bnDef.getVersion());
 
-    // 3. Get the factory
+    // Getting the factory
     let factory = bnDef.getFactory();
-    
-    // 4. Lets create a new Resource of type = Transaction
-    //    Here is the sample data
-    // {
-    //     "$class": "org.acme.airline.flight.CreateFlight",
-    //     "flightNumber": "AE101-06-06-2019",
-    //     "origin": "MSP",
-    //     "destination": "SEA",
-    //     "schedule": "2019-06-06T18:49:58.273Z"
-    // }
-
-    // 4. Create an instance of transaction
+   
+    // Create an instance of transaction
     let options = {
         generate: false,
         includeOptionalFields: false
     }
+    // Tring on demo id
     let organId = "AE105-05-06-2019";
     let transaction = factory.newTransaction(namespace,transactionType,organId,options);
 
-    // 5. Set up the properties of the transaction object
+    // Setting up the properties of the transaction object
    
     transaction.setPropertyValue('organId','3344');
     transaction.setPropertyValue('type',1);
@@ -78,7 +42,7 @@ function main(error){
     transaction.setPropertyValue('donorId' , 101);
 
 
-    // 6. Submit the transaction
+    // Submitting the transaction
     return bnUtil.connection.submitTransaction(transaction).then(()=>{
         console.log("6. Transaction Submitted/Processed Successfully!!")
 
@@ -90,30 +54,3 @@ function main(error){
         bnUtil.disconnect();
     });
 }
-
-
-
-
-/**
- * Test Data for adding flight in registry
- {
-  "$class": "org.acme.airline.flight.Flight",
-  "flightId": "AE101-05-05-2019",
-  "flightNumber": "AE101",
-  "route": {
-    "$class": "org.acme.airline.flight.Route",
-    "origin": "ATL",
-    "destination": "EWR",
-    "schedule": "2019-12-17T18:49:58.288Z",
-    "id": "string"
-  }
-}
-* Adding flight using the createFlight transaction
-{
-  "$class": "org.acme.airline.flight.CreateFlight",
-  "flightNumber": "AE101-06-06-2019",
-  "origin": "MSP",
-  "destination": "SEA",
-  "schedule": "2019-06-06T18:49:58.273Z"
-}
-*/
